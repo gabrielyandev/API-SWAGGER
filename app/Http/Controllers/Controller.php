f3f5122a0815d728462c638d5c415e8fc7a4ebf8
Gabriel Yan <yanjob.dev@gmail.com>
2026-09-03 10:28:22 -0300

    fix: dominio

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use OpenApi\Attributes as OA;
 
 #[OA\Info(version: "1.0.0", title: "API de Itens", description: "Documentação da API de Itens para o ambiente Hostinger")]
 #[OA\Server(url: 'http://localhost:8000', description: 'Servidor Local')]
-#[OA\Server(url: 'https://seu-dominio.com', description: 'Servidor de Produção')]
+#[OA\Server(url: 'https://api.suporteourobras.com', description: 'Servidor de Produção')]
 abstract class Controller
 {
     //
